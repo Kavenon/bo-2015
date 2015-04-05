3a3c8427c9daf10b862b0a2ecb466905c666eac0
unknown <kavenon@gmail.com>
2015-04-05 10:19:07 +0200

    first graph generated

--- src/main/java/pl/agh/edu/bo/projekt/SimpleGraphView.java
+++ src/main/java/pl/agh/edu/bo/projekt/SimpleGraphView.java
@@ -4,46 +4,62 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 
-/**
- *
- * @author Dr. Greg M. Bernstein
- */
-public class SimpleGraphView {
-    Graph<Integer, String> g;
-    /** Creates a new instance of SimpleGraphView */
-    public SimpleGraphView() {
-        // Graph<V, E> where V is the type of the vertices and E is the type of the edges
-        g = new SparseMultigraph<Integer, String>();
-        // Add some vertices. From above we defined these to be type Integer.
-        g.addVertex((Integer)1);
-        g.addVertex((Integer)2);
-        g.addVertex((Integer)3); 
-        // Note that the default is for undirected edges, our Edges are Strings.
-        g.addEdge("Edge-A", 1, 2); // Note that Java 1.5 auto-boxes primitives
-        g.addEdge("Edge-B", 2, 3);  
-    }
-    
 
-    public static void main(String[] args) {
-        SimpleGraphView sgv = new SimpleGraphView(); //We create our graph in here
-        // The Layout<V, E> is parameterized by the vertex and edge types
-        Layout<Integer, String> layout = new CircleLayout(sgv.g);
-        layout.setSize(new Dimension(300,300)); // sets the initial size of the layout space
-        // The BasicVisualizationServer<V,E> is parameterized by the vertex and edge types
-        BasicVisualizationServer<Integer,String> vv = new BasicVisualizationServer<Integer,String>(layout);
-        vv.setPreferredSize(new Dimension(350,350)); //Sets the viewing area size
-        
-        JFrame frame = new JFrame("Simple Graph View");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(vv); 
-        frame.pack();
-        frame.setVisible(true);       
-    }
-    
+public class SimpleGraphView {
+
+	private int n;
+	private double p;
+
+	public SimpleGraphView(int n, double p) {
+		this.n = n;
+		this.p = p;
+	}
+
+	public Graph<Integer, String> generateRandomGraph() {
+
+		DirectedSparseGraph<Integer, String> dg = new DirectedSparseGraph<Integer, String>();
+		for (int i = 0; i < n; i++) {
+			dg.addVertex(i);
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (Math.random() < p) {
+					dg.addEdge(i + "-to-" + j, i, j);
+				}
+			}
+		}
+		return dg;
+	}
+
+	public static void main(String[] args) {
+
+		SimpleGraphView rgg = new SimpleGraphView(20, 0.5);
+
+		// int maxEdgeCount = (n*(n-1))>>1;
+		Graph<Integer, String> g = rgg.generateRandomGraph();
+
+		// Add some edges. From above we defined these to be of type String
+		// Note that the default is for undirected edges.
+		Layout<Integer, String> layout = new KKLayout<Integer, String>(g);
+		layout.setSize(new Dimension(1200, 600)); // sets the initial size of
+		// the space
+		// The BasicVisualizationServer<V,E> is parameterized by the edge types
+		BasicVisualizationServer<Integer, String> vv = new BasicVisualizationServer<Integer, String>(
+				layout);
+		vv.setPreferredSize(new Dimension(1200, 600)); // Sets the viewing area
+		// size
+		JFrame frame = new JFrame("Simple Graph View");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(vv);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
 }
